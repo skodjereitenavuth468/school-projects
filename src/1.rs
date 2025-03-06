1fcbe25b01a8fdb7301cdb2f9bf83105a6b87208
skodjereitenavuth468 <skodjereitenavuth468@gmail.com>
2025-03-06 02:11:24 +0000

    Add 1.rs

---
+++ src/1.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert("key1", "value1");
+    map.insert("key2", "value2");
+    map.insert("key3", "value3");
+
+    for (k, v) in &map {
+        println!("{}: {}", k, v);
+    }
+}
